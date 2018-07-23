0641105ce271c2a711d7d41556e875a4d736d669
david <david.journo.31@gmail.com>
2018-07-23 13:59:31 +0200

    Exercice_5_Terminé

---
+++ Function/exercice_5.php
@@ -0,0 +1,8 @@
+<?php
+/* ##Exercice 5 Faire une fonction qui prend en paramètre un nombre 
+et une chaine de caractères et qui renvoit la concaténation de ces deux paramètres.*/
+function strings_and_numbers($nb, $str){
+    echo $nb . $str;
+}
+strings_and_numbers(2, "salut");
+?>
