0201194f071c50ec0a895ff06a827a5498b9dedf
david <david.journo.31@gmail.com>
2018-07-23 13:54:17 +0200

    Exercice_2_Terminé

---
+++ Function/exercice_2.php
@@ -0,0 +1,8 @@
+<?php
+/* ##Exercice 2 Faire une fonction qui prend en paramètre une chaine de caractères 
+et qui retourne cette même chaine. */
+function strings($str){
+    echo $str;
+}
+strings("bla bla");
+?>
