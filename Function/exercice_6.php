287475917156269b44e56d0f22ff20cb1407b4a8
david <david.journo.31@gmail.com>
2018-07-23 14:38:31 +0200

    Exercice_6_Terminé

--- Function/exercice_6.php
+++ Function/exercice_6.php
@@ -2,7 +2,7 @@
 /* ##Exercice 6 Faire une fonction qui prend trois paramètres : nom, prenom et age. Elle doit renvoyer une chaine de la forme :
 "Bonjour" + nom + prenom + ",tu as" + age + "ans". */
 function hello($nom, $prenom, $age){
-    echo "Bonjour " . $nom . " " . $prenom . ", tu as " . $age . " ans.";
+    echo "Bonjour $nom $prenom, tu as $age ans.";
 }
 hello("Tom", "Turner", 29);
 ?>
