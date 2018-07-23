e48a9150c971200d453878783e4030a2c94aecb1
david <david.journo.31@gmail.com>
2018-07-23 10:15:05 +0200

    Exercice 1_Terminé

---
+++ Loop/exercice_1.php
@@ -0,0 +1,10 @@
+<?php 
+
+/* #PHP - Les boucles ##Exercice 1 Créer une variable et l'initialiser à 0.
+Tant que cette variable n'atteint pas 10, il faut :
+
+    l'afficher
+    l'incrementer */
+
+for ($i = 0; $i <= 10; $i++)
+echo $i;?>
---
+++ Loop/exercice_2.php
@@ -0,0 +1,13 @@
+<?php 
+/* ##Exercice 2 Créer deux variables. Initialiser la première à 0 et la deuxième avec un nombre compris en 1 et 100.
+Tant que la première variable n'est pas supérieur à 20 :
+
+    multiplier la première variable avec la deuxième
+    afficher le résultat
+    incrementer la première variable
+*/
+$nb = 12;
+$i = 0;
+while ($i < 20)
+echo $i++
+?>
