194a68d091b355b68e36e74ec58ef42173d88f34
david <david.journo.31@gmail.com>
2018-07-23 11:01:49 +0200

    Exercice_3_En_Cours

---
+++ Loop/exercice_3.php
@@ -0,0 +1,15 @@
+<?php
+/*
+##Exercice 3 Créer deux variables. Initialiser la première à 100 
+et la deuxième avec un nombre compris en 1 et 100.
+Tant que la première variable n'est pas inférieur ou égale à 10 :
+
+    multiplier la première variable avec la deuxième
+    afficher le résultat
+    décrémenter la première variable
+ */
+$i = 100;
+$nb = 20;
+while ($i <= 10)
+echo ' '. $i-- * $nb;
+?>
