1748f8ae02c1106d9e05811e0c6abb105283b2c0
david <david.journo.31@gmail.com>
2018-07-23 13:54:57 +0200

    Exercice_3_Terminé

---
+++ Function/exercice_3.php
@@ -0,0 +1,8 @@
+<?php 
+/* ##Exercice 3 Faire une fonction qui prend en paramètre deux chaines de caractères 
+et qui revoit la concaténation de ces deux chaines */
+function add_strings($str, $str2){
+    echo $str . $str2;
+}
+add_strings("giraffe", "jaune");
+?>
