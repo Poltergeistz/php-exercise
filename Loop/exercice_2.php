5177abcb745541207b71e75b8c0d33257390dfbc
david <david.journo.31@gmail.com>
2018-07-23 11:40:21 +0200

    Exercice_2_Terminé

--- Loop/exercice_2.php
+++ Loop/exercice_2.php
@@ -1,5 +1,5 @@
 <?php 
-/* ##Exercice 2 Créer deux variables. Initialiser la première à 0 et la deuxième avec un nombre compris en 1 et 100.
+/*  ##Exercice 2 Créer deux variables. Initialiser la première à 0 et la deuxième avec un nombre compris en 1 et 100.
 Tant que la première variable n'est pas supérieur à 20 :
 
     multiplier la première variable avec la deuxième
