05dbceadcf425422ed1f9ce6f279f239dafc12b9
david <david.journo.31@gmail.com>
2018-07-23 14:07:39 +0200

    Exercice_8_Terminé

---
+++ Function/exercice_8.php
@@ -0,0 +1,9 @@
+<?php
+/* ##Exercice 8 Faire une fonction qui prend en paramètre trois nombres 
+et qui renvoit la somme de ces nombres.
+Tous les paramètres doivent avoir une valeur par défaut. */
+function addition($nb1 = 1, $nb2 = 3, $nb3 = 2){
+    echo $nb1 + $nb2 + $nb3;
+}
+addition();
+?>
