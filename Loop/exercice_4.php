514f51babfdcc61c2e0db9b24dcb6be1eea168d0
david <david.journo.31@gmail.com>
2018-07-23 11:09:47 +0200

    Exercice_4_Terminé

---
+++ Loop/exercice_4.php
@@ -0,0 +1,12 @@
+<?php
+/*
+##Exercice 4 Créer une variable et l'initialiser à 1.
+Tant que cette variable n'atteint pas 10, il faut :
+
+    l'afficher
+    l'incrementer de la moitié de sa valeur
+ */
+$i = 1;
+for ($i = 1; $i <= 10; $i += 0.5)
+echo ' '. $i;
+?>
