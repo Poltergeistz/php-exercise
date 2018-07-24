4ed8ffa83e7cf0bbe854748629023d6a256a961d
david <david.journo.31@gmail.com>
2018-07-24 16:52:00 +0200

    Exercice_1_Terminé

---
+++ Form/Exercice_1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Exercice 1</title>
+ </head>
+ <body>
+ <form action="/user.php" method="get">
+ Nom : <input type="text" name="nom">
+ Prenom : <input type="text" name="prenom">
+<input type="submit">
+ </form>
+ <?php
+/* #PHP - Les formulaires Vous devez agrémenter votre code avec de l'HTML 5 valide (DOCTYPE, ....) 
+##Exercice 1 Créer un formulaire demandant le nom et le prénom. 
+Ce formulaire doit rediriger vers la page user.php avec la méthode GET. */
+?>
+</body>
+</html>
