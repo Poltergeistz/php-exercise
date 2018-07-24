54fbe1ba36e846c1cb4e77e5f1067ea57ed3027b
david <david.journo.31@gmail.com>
2018-07-24 11:49:59 +0200

    Exercice_9_Terminé

---
+++ Array/exercice_9.php
@@ -0,0 +1,11 @@
+<?php
+/* ##Exercice 9 Avec le tableau de l'exercice 5, afficher toutes les valeurs de ce tableau.*/
+$departements = array(
+    02 => 'Aisne',
+    59 => 'Nord',
+    60 => 'Oise',
+    62 => 'Pas-de-Calais',
+    80 => 'Somme',
+);
+echo var_dump($departements);
+?>
