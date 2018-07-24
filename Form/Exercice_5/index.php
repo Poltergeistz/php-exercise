174c0071aa5697fe813534669dab20a58d8eb0b3
david <david.journo.31@gmail.com>
2018-07-24 21:46:55 +0200

    Exercice_5_Terminé

---
+++ Form/Exercice_5/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Exercice 5</title>
+ </head>
+ <body>
+ <form action="/index.php" method="post">
+ <select name="civilité">
+ <option> Mr
+ <option> Mme
+ </select>
+    Nom : <input type="text" name="nom">
+    Prenom : <input type="text" name="prenom">
+    <input type="submit">
+    </form>
+ <?php
+ /*
+##Exercice 5 Créer un formulaire sur la page index.php avec :
+
+Une liste déroulante pour la civilité (Mr ou Mme)
+Un champ texte pour le nom
+Un champ texte pour le prénom
+
+Ce formulaire doit rediriger vers la page index.php.
+Vous avez le choix de la méthode. */
+?>
+</body>
+</html>
