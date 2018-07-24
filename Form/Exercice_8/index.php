c759eebaa6f4318e8493839f797daffa5ae75357
david <david.journo.31@gmail.com>
2018-07-24 21:53:26 +0200

    Exercice_8_Terminé

---
+++ Form/Exercice_8/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Exercice 8</title>
+ </head>
+ <body>
+ <?php
+ /*
+##Exercice 8 Sur le formulaire de l'exercice 6, 
+en plus de ce qui est demandé sur les exercices précédent, 
+vérifier que le fichier transmis est bien un fichier pdf. 
+*/
+if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['civilité']) && pathinfo($_FILES["file"]["filename"], PATHINFO_EXTENSION) == "pdf"){
+  echo strip_tags($_POST['civilité'] . " " . $_POST['nom'] . " " . $_POST["prenom"] . "\n" . pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION));
+}
+else{ ?>
+<form action="/index.php" method="post" enctype="multipart/form-data">
+<select name="civilité">
+<option> Mr
+<option> Mme
+</select>
+  Nom : <input type="text" name="nom">
+  Prenom : <input type="text" name="prenom">
+  Upload : <input type="file" name="filename">
+  <input type="submit">
+  </form>
+<?php
+}
+?>
+</body>
+</html>
