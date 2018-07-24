51f0abbbdff5a4eb69612d3a12ff4290644ddec4
david <david.journo.31@gmail.com>
2018-07-24 11:00:03 +0200

    Exercice_6_Terminé_2_Méthodes

--- Array/exercice_6.php
+++ Array/exercice_6.php
@@ -1,11 +1,11 @@
 <?php
 /* ##Exercice 6 Avec le tableau de l'exercice 5, afficher la valeur de l'index 59.*/
-$departements = array(
+$departements = [
     02 => 'Aisne',
     59 => 'Nord',
     60 => 'Oise',
     62 => 'Pas-de-Calais',
-    80 => 'Somme',
-);
+    80 => 'Somme'
+];
 echo $departements[59];
 ?>
