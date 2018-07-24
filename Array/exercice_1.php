6950ed1d4df77461d83ffa78fa4070201d5f641f
david <david.journo.31@gmail.com>
2018-07-24 10:18:27 +0200

    Exercice_1_Terminé

---
+++ Array/exercice_1.php
@@ -0,0 +1,19 @@
+<?php
+/* #PHP - Les tableaux ##Exercice 1 Créer un tableau mois et l'initialiser avec les valeurs suivantes :
+
+janvier
+février
+mars
+avril
+mai
+juin
+juillet
+aout
+septembre
+octobre
+novembre
+décembre */
+
+$mois = ['janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre'];
+// var_dump($mois);
+?>
