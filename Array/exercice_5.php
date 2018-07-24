b9a589f8c721210629a8b4eda0cefa9637a2f57c
david <david.journo.31@gmail.com>
2018-07-24 10:56:22 +0200

    Exercice_5_Terminé

---
+++ Array/exercice_5.php
@@ -0,0 +1,12 @@
+<?php
+/* ##Exercice 5 Créer un tableau associatif avec comme index le numéro des départements
+ des Hauts de France et en valeur leur nom. */
+$departements = array(
+    02 => 'Aisne',
+    59 => 'Nord',
+    60 => 'Oise',
+    62 => 'Pas-de-Calais',
+    80 => 'Somme',
+);
+echo var_dump($departements);
+?>
