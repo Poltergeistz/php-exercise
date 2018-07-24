af746054567261835c1bfc4074508fb857fc5532
david <david.journo.31@gmail.com>
2018-07-24 10:23:43 +0200

    Exercice_3_Terminé

---
+++ Array/exercice_3.php
@@ -0,0 +1,6 @@
+<?php
+/* ##Exercice 3 Avec le tableau de l'exercice , afficher la valeur de l'index 5. */
+$mois = ['janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre'];
+echo $mois[5];
+?>
+
