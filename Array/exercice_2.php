e2ed6a666ca1e2c71e5e8e09124bc370a1d379be
david <david.journo.31@gmail.com>
2018-07-24 10:20:58 +0200

    Exercice_2_Terminé

---
+++ Array/exercice_2.php
@@ -0,0 +1,6 @@
+<?php
+/* ##Exercice 2 Avec le tableau de l'exercice 1, afficher la valeur de la troisième ligne de ce tableau.
+*/
+$mois = ['janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre'];
+echo $mois[2];
+?>
