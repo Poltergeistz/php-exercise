702b81790b318f0987b9ecabd25e003e8c7d8065
david <david.journo.31@gmail.com>
2018-07-24 16:52:21 +0200

    Exercice_2_Terminé

---
+++ Form/Exercice_2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Exercice 1</title>
+ </head>
+ <body>
+ <form action="/user.php" method="post">
+ Nom : <input type="text" name="nom">
+ Prenom : <input type="text" name="prenom">
+<input type="submit">
+</form>
+<?php
+/* ##Exercice 2 Créer un formulaire demandant le nom et le prénom.
+Ce formulaire doit rediriger vers la page user.php avec la méthode POST. */
+?>
+</body>
+</html>
