b9f8cabd5001da92bfa157e7d50d4960f9eae326
david <david.journo.31@gmail.com>
2018-07-24 13:37:14 +0200

    Exercice_8_Terminé

---
+++ Array/exercice_8.php
@@ -0,0 +1,7 @@
+<?php
+/* ##Exercice 8 Avec le tableau de l'exercice 1 et une boucle, 
+afficher toutes les valeurs de ce tableau. */
+$mois = ['janvier','février','mars','avril','mai','juin','juillet','août','septembre','octobre','novembre','décembre'];
+for ($i = 0; $i <sizeof($mois); $i++)
+echo ' ' . $mois[$i];
+?>
