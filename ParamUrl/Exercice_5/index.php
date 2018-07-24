f1b7565987778b0b6781333efa59ec1a2eeab2f5
david <david.journo.31@gmail.com>
2018-07-24 15:42:34 +0200

    Exercice_5_Terminé

---
+++ ParamUrl/Exercice_5/index.php
@@ -0,0 +1,18 @@
+<html>
+ <head>
+  <title>Exercice 5</title>
+ </head>
+ <body>
+ <?php
+/* ##Exercice 5 Faire une page index.php.
+Tester sur cette page que tous les paramètres de cette URL existent et les afficher:
+index.php?semaine=12 */
+if(isset($_GET['semaine'])) {
+    echo $_GET['semaine'];
+    }
+    if(!isset($_GET['semaine'])) {
+    echo "le querystring ne contient pas la bonne semaine";
+    }
+?>
+</body>
+</html>
