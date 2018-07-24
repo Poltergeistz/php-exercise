a121dfc6fddbcf8786eaae0c14dedbc2e144fd70
david <david.journo.31@gmail.com>
2018-07-24 13:26:14 +0200

    Exercice_4_Terminé

---
+++ Array/exercice_4.php
@@ -0,0 +1,7 @@
+<?php
+/* ##Exercice 4 Avec le tableau de l'exercice 1, 
+modifier le mois de aout pour lui ajouter l'accent manquant. */
+$mois = ['janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre'];
+$mois[7] = 'août';
+echo print_r($mois);
+?>
