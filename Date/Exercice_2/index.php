558e5277fe86a979f0e985080aedca68f6910ea8
david <david.journo.31@gmail.com>
2018-07-25 11:24:06 +0200

    Exercice_2_Terminé

---
+++ Date/Exercice_2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+/* 
+##Exercice 2 Afficher la date courante en respectant la forme jj-mm-aa (ex : 16-05-16) */
+echo date("d-m-y")
+?>
+</body>
