249f3351a377a9a24a837de063123c3200f9e48f
david <david.journo.31@gmail.com>
2018-07-25 11:29:27 +0200

    Exercice_1_Terminé

--- Date/Exercice_1/index.php
+++ Date/Exercice_1/index.php
@@ -12,7 +12,7 @@
 /* #PHP - Les dates Pour tous les exercices, vous pouvez utiliser le site http://php.net
 N'hesitez pas à faire des pages avec du HTML/CSS 
 ##Exercice 1 Afficher la date courante en respectant la forme jj/mm/aaaa (ex : 16/05/2016) */
-echo date("d/m/Y")
+echo date("d/m/Y");
 ?>
 </body>
 </html>
