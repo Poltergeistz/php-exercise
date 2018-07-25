b237016d477c40db25524199af935ea3791552d8
david <david.journo.31@gmail.com>
2018-07-25 13:11:36 +0200

    Exercice_4_Terminé

---
+++ Date/Exercice_4/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+/* ##Exercice 4 Afficher le timestamp du jour.
+Afficher le timestamp du mardi 2 août 2016 à 15h00. */
+echo date(strtotime('02/08/2016 15:00:00'));
+?>
+</body>
+</html>
