6200ac06919ddadd0f31e191c607a9e190bbf026
david <david.journo.31@gmail.com>
2018-07-25 10:41:40 +0200

    Exercice_1_Terminé

---
+++ GlobalVariable/Exercice_1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+/* #PHP - Variables superglobales, sessions et cookies Dans tous les exercices, il faudra faire une page HTML 5 valide et soigner son CSS. ##Exercice 1 Faire une page HTML permettant de donner à l'utilisateur :
+
+son User Agent
+son adresse ip
+le nom du serveur
+*/
+echo $_SERVER['HTTP_USER_AGENT'];
+?> 
+</body>
+</html>
+
+
+
