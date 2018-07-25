1eef7609c3630c7321d5749c213d8a837df5c679
david <david.journo.31@gmail.com>
2018-07-25 11:20:26 +0200

    Exercice_1_Terminé

---
+++ Date/Exercice_1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+/* #PHP - Les dates Pour tous les exercices, vous pouvez utiliser le site http://php.net
+N'hesitez pas à faire des pages avec du HTML/CSS 
+##Exercice 1 Afficher la date courante en respectant la forme jj/mm/aaaa (ex : 16/05/2016) */
+echo date("d/m/Y")
+?>
+</body>
+</html>
