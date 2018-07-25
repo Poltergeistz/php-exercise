c7ae76d25d69d871ba2d123c4a913cb64c9f593c
david <david.journo.31@gmail.com>
2018-07-25 11:21:31 +0200

    Exercice_2_Terminé

---
+++ GlobalVariable/Exercice_2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Document</title>
+</head>
+<body>
+<?php 
+/* ##Exercice 2 Sur la page index, faire un liens vers une autre page.
+Passer d'une page à l'autre le contenu des variables nom, prenom et age grâce aux sessions.
+Ces variables auront été définies directement dans le code.
+Il faudra afficher le contenu de ces variables sur la deuxième page.*/
+?> 
+</body>
+</html>
+
