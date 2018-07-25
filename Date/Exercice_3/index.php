2c108e7d177ff009065fd32644616f80ee07ded2
david <david.journo.31@gmail.com>
2018-07-25 11:29:39 +0200

    Exercice_3_Terminé

---
+++ Date/Exercice_3/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+/* ##Exercice 3 Afficher la date courante avec le jour de la semaine et le mois en toutes lettres 
+(ex : mardi 2 août 2016)
+Bonus : Le faire en français.*/
+echo date("l j F Y");
+?>
+</body>
+</html>
+
