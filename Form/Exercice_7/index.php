cbbf7c973cc4490709da7dd16d88eea784608e41
david <david.journo.31@gmail.com>
2018-07-26 13:09:39 +0200

    Exercice_7_En_cours

--- Form/Exercice_7/index.php
+++ Form/Exercice_7/index.php
@@ -12,19 +12,19 @@
 ##Exercice 7 Au formulaire de l'exercice 5, ajouter un champ d'envoi de fichier. 
 Afficher en plus de ce qui est demandé à l'exercice 6, le nom et l'extension du fichier.
 */
-    if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['civilité']) && pathinfo($_FILES["file"]["filename"], PATHINFO_EXTENSION) == "pdf"){
-        echo strip_tags($_POST['civilité'] . " " . $_POST['nom'] . " " . $_POST["prenom"]);
-        echo strip_tags("File : " . pathinfo($_FILES["file"]["filename"], PATHINFO_FILENAME));
+    if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['civilité']) && pathinfo($_POST['filename'], PATHINFO_EXTENSION) == 'pdf'){
+        $path = "File : " . pathinfo($_POST['filename'], PATHINFO_FILENAME);
+        echo strip_tags($_POST['civilité'] . " " . $_POST['nom'] . " " . $_POST["prenom"] . " " . $path);
     }
     else{ ?>
- <form action="/index.php" method="post" enctype="multipart/form-data">
+ <form action="/index.php" method="POST" enctype="multipart/form-data">
  <select name="civilité">
  <option> Mr
  <option> Mme
  </select>
     Nom : <input type="text" name="nom">
     Prenom : <input type="text" name="prenom">
-    Upload : <input type="file" name="filename">
+    Upload : <input type="file" name="filename" id="filename">
     <input type="submit">
     </form>
     <?php }
